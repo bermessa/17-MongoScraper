9cdf8daae351dbfd8ae92a62e5fd02fcc2b858f0
Teddy Bermes <bermessa@gmail.com>
2018-01-09 19:41:07 +0000

    heroku addition

--- server.js
+++ server.js
@@ -14,14 +14,18 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Set Heroku
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
 //Set Mongoose
 var mongoose = require("mongoose");
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/scraper');
+mongoose.connect(MONGODB_URI);
 
 //Routes
 var routes = require("./controllers/controller.js");
 app.use("/", routes);
+
 
 // Start the server
 app.listen(PORT, function() {
